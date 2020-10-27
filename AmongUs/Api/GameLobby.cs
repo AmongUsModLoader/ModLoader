f7cea04ec26ffb5fa3341f671227d63c0976af79
George Kazanjian <soggymustacheofficialmods@gmail.com>
2020-10-26 22:55:09 -0400

    Fix game lobby naming and add more to wrapper

--- AmongUs/Api/GameLobby.cs
+++ AmongUs/Api/GameLobby.cs
@@ -1,37 +1,37 @@
 using System;
 
 namespace AmongUs.Api {
-	public static class GameLobbyEvents
+	public static class GameLobby
 	{
 		public static event Action<IGameLobby> LobbyLoadEvent;
-		public static event Action<GameStartManager, float?> SetStartCounterEventPost;
-		public static event Action<GameStartManager, float?> SetStartCounterEventPre;
-		public static event Action<GameStartManager> UpdateEvent;
-		public static event Action<GameStartManager> MakePublicEvent;
+		public static event Action<IGameLobby, float?> SetStartCounterEventPost;
+		public static event Action<IGameLobby, float?> SetStartCounterEventPre;
+		public static event Action<IGameLobby> UpdateEvent;
+		public static event Action<IGameLobby> MakePublicEvent;
 		/// <summary>
 		/// Called when the game tries the start. EX: Start button is pressed
 		/// </summary>
-		public static event Action<GameStartManager> TryStartEvent;
+		public static event Action<IGameLobby> TryStartEvent;
 		/// <summary>
 		/// Called when the game tries the start while meeting player requirements.
 		/// </summary>
-		public static event Action<GameStartManager, bool> GameStartingEvent;
+		public static event Action<IGameLobby, bool> GameStartingEvent;
 
 		
 		//"Load", this is when the lobby loads, not when the game starts
 		public static void Start(IGameLobby manager) => LobbyLoadEvent?.Invoke(manager);
 		
-		public static void SetStartCounterPost(GameStartManager manager, float value) => SetStartCounterEventPost?.Invoke(manager, value);
+		public static void SetStartCounterPost(IGameLobby manager, float value) => SetStartCounterEventPost?.Invoke(manager, value);
 		
-		public static void SetStartCounterPre(GameStartManager manager, float value) => SetStartCounterEventPre?.Invoke(manager, value);
+		public static void SetStartCounterPre(IGameLobby manager, float value) => SetStartCounterEventPre?.Invoke(manager, value);
 
-		public static void Update(GameStartManager manager) => UpdateEvent?.Invoke(manager);
+		public static void Update(IGameLobby manager) => UpdateEvent?.Invoke(manager);
 		
-		public static void MakePublic(GameStartManager manager) => MakePublicEvent?.Invoke(manager);
+		public static void MakePublic(IGameLobby manager) => MakePublicEvent?.Invoke(manager);
 
-		public static void TryStart(GameStartManager manager) => TryStartEvent?.Invoke(manager);
+		public static void TryStart(IGameLobby manager) => TryStartEvent?.Invoke(manager);
 
-		public static void GameStarting(GameStartManager manager, bool b) => GameStartingEvent?.Invoke(manager, b);
+		public static void GameStarting(IGameLobby manager, bool b) => GameStartingEvent?.Invoke(manager, b);
 	}
 
 	public interface IGameLobby 
--- ModLoaderClient/Patches/GameStartManagerPatches.cs
+++ ModLoaderClient/Patches/GameStartManagerPatches.cs
@@ -7,7 +7,7 @@ namespace AmongUs.Client.Loader.Patches {
 		
 		[HarmonyPatch(typeof(GameStartManager), "Start")]
 		private static class GameStartPatch {
-			public static void Postfix(GameStartManager __instance) => GameLobbyEvents.Start(new GameLobbyWrapper(__instance));
+			public static void Postfix(GameStartManager __instance) => GameLobby.Start(new GameLobbyWrapper(__instance));
 			
 		}
         
@@ -15,33 +15,33 @@ namespace AmongUs.Client.Loader.Patches {
 		private static class GameBeginPatch
 		{
 			//Called when the start button is pressed. Even if there arent enough players
-			public static void Postfix(GameStartManager __instance) => GameLobbyEvents.TryStart(__instance);
+			public static void Postfix(GameStartManager __instance) => GameLobby.TryStart(new GameLobbyWrapper(__instance));
 		}
         
 		[HarmonyPatch(typeof(GameStartManager), "ReallyBegin")]
 		private static class ReallyBeginPatch
 		{
 			//Called when the start button is pressed. Only if enough players
-			public static void Postfix(GameStartManager __instance, [HarmonyArgument(0)] bool canStart) => GameLobbyEvents.GameStarting(__instance, canStart);
+			public static void Postfix(GameStartManager __instance, [HarmonyArgument(0)] bool canStart) => GameLobby.GameStarting(new GameLobbyWrapper(__instance), canStart);
 		}
 
 		[HarmonyPatch(typeof(GameStartManager), "SetStartCounter")]
 		private static class StartCountDownPatch {
-			public static void Prefix(GameStartManager __instance, [HarmonyArgument(0)] sbyte value) => GameLobbyEvents.SetStartCounterPre(__instance, value);
+			public static void Prefix(GameStartManager __instance, [HarmonyArgument(0)] sbyte value) => GameLobby.SetStartCounterPre(new GameLobbyWrapper(__instance), value);
             
-			public static void Postfix(GameStartManager __instance, [HarmonyArgument(0)] sbyte value) => GameLobbyEvents.SetStartCounterPost(__instance, value);
+			public static void Postfix(GameStartManager __instance, [HarmonyArgument(0)] sbyte value) => GameLobby.SetStartCounterPost(new GameLobbyWrapper(__instance), value);
 		}
         
 		[HarmonyPatch(typeof(GameStartManager), "Update")]
 		private static class LobbyUpdatePatch
 		{
-			public static void Postfix(GameStartManager __instance) => GameLobbyEvents.Update(__instance);
+			public static void Postfix(GameStartManager __instance) => GameLobby.Update(new GameLobbyWrapper(__instance));
 		}
         
 		[HarmonyPatch(typeof(GameStartManager), "MakePublic")]
 		private static class MakePublicPatch
 		{
-			public static void Postfix(GameStartManager __instance) => GameLobbyEvents.MakePublic(__instance);
+			public static void Postfix(GameStartManager __instance) => GameLobby.MakePublic(new GameLobbyWrapper(__instance));
 		}
 	}
 }
